74a81300c49fa4e8fd3e712df884f490bd6a3632
Danisole <danisole82@gmail.com>
2023-05-07 00:19:17 -0300

    nuevamente readme

--- proyecto_py/AppCoderSocial/models.py
+++ proyecto_py/AppCoderSocial/models.py
@@ -37,7 +37,7 @@ class Relationship(models.Model):
     to_user = models.ForeignKey(User, related_name='related_to', on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.from_user} to { self.to_user}'
+        return f'{self.from_user} to {self.to_user}'
     
     class Meta:
         indexes = [
